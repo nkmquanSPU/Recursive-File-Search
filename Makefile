4db8d732c73151f59a559481ce4705ce35283221
nkmquanSPU <nkmquan@spu.edu>
2018-05-19 17:35:36 -0700

    Program can receive the search term and starting directory from user

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc -o file_search file_search.c
+clean:
+	rm -rf file_search
+
---
+++ file_search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	// if user enters invalid commmand
+	if(argc != 3)
+	{
+		printf("Usage: ./file_search <search term> <starting directory>\n"); // display program's usage
+		exit(1);
+	}	
+	char search_term[256];
+	char start_dir[256];
+
+	strcpy(search_term, argv[1]); // get the search term from user
+	strcpy(start_dir, argv[2]); // get the starting directory from user
+
+	return 0;
+}
